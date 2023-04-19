ca5910f701cd883cde57869a9d13ba108548d468
Valerie Tovar <valeriectb26@gmail.com>
2023-04-18 21:03:06 -0500

    Organización del proyecto

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+    const inicioTemplate = Handlebars.templates.inicio;
+    $("#main").html(inicioTemplate());
+});
+
+function clickProduct () {
+    const productTemplate = Handlebars.templates.product;
+    $("#main").html(productTemplate());
+
+    console.log('dasdsad');
+}
+
+$('#btnMenu').on('click', () => {
+    const menu = $('#menu');
+
+    console.log(menu.hasClass('menu-show'));
+
+    if (!menu.hasClass('menu-show')) {
+        $('#menu').addClass('menu-show');
+    } else {
+        $('#menu').removeClass('menu-show');
+    }
+});
+
+$('#btnNavbar').on('click', () => {
+    const navbar = $('#navbar');
+    const mainContainer = $('#mainContainer');
+    
+    if (!navbar.hasClass('navbar-collapse')) {
+        navbar.addClass('navbar-collapse');
+        mainContainer.addClass('container-navbar-collapse');
+    } else {
+        navbar.removeClass('navbar-collapse');
+        mainContainer.removeClass('container-navbar-collapse');
+    }
+})
